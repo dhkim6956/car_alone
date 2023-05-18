2d353f305595eda1af8f85e9a87bde2858773822
dhkim6956 <dhkim6956@naver.com>
2023-05-18 17:48:56 +0900

    sense data streaming

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from Raspi_MotorHAT import Raspi_MotorHAT, Raspi_DCMotor
 from Raspi_PWM_Servo_Driver import PWM
 import mysql.connector
-from threading import Timer
+from threading import Timer, Lock
 from time import sleep
 import signal
 import sys
 from sense_hat import SenseHat
 from time import sleep
+import datetime
 
 def closeDB(signal, frame):
     print("BYE")
@@ -20,6 +21,7 @@ def closeDB(signal, frame):
 def polling():
     global cur, db, ready
     
+    lock.acquire()
     cur.execute("select * from command order by time desc limit 1")
     for (id, time, cmd_string, arg_string, is_finish) in cur:
         if is_finish == 1 : break
@@ -27,10 +29,38 @@ def polling():
         cur.execute("update command set is_finish=1 where is_finish=0")
 
     db.commit()
+    lock.release()
      
     global timer
     timer = Timer(0.1, polling)
     timer.start()
+
+def sensing():
+    global cur, db, sense
+
+    pressure = sense.get_pressure()
+    temp = sense.get_temperature()
+    humidity = sense.get_humidity()
+
+    time = datetime.datetime.now()
+    num1 = round(pressure / 10000, 3)
+    num2 = round(temp / 100, 2)
+    num3 = round(humidity / 100, 2)
+    meta_string = '0|0|0'
+    is_finish = 0
+
+    print(num1, num2, num3)
+    query = "insert into sensing(time, num1, num2, num3, meta_string, is_finish) values (%s, %s, %s, %s, %s, %s)"
+    value = (time, num1, num2, num3, meta_string, is_finish)
+
+    lock.acquire()
+    cur.execute(query, value)
+    db.commit()
+    lock.release()
+
+    global timer2
+    timer2 = Timer(1, sensing)
+    timer2.start()
 
 def go():
     myMotor.setSpeed(200)
@@ -45,21 +75,13 @@ def stop():
     myMotor.run(Raspi_MotorHAT.RELEASE)
 
 def left():
-    pwm.setPWM(0, 0, 300)
+    pwm.setPWM(0, 0, 330)
 
 def mid():
-    pwm.setPWM(0, 0, 390)
+    pwm.setPWM(0, 0, 370)
 
 def right():
     pwm.setPWM(0, 0, 440)
-
-def sensing():
-    global cur, db
-    print("SENSING!")
-
-    global timer2
-    timer2 = Timer(1, sensing)
-    timer2.start()
 
 #init
 db = mysql.connector.connect(host='52.79.105.4', user='rccar', password='1', database='senseDB', auth_plugin='mysql_native_password')
@@ -67,13 +89,14 @@ cur = db.cursor()
 ready = None
 timer = None
 
-mh = Raspi_MotorHAT(addr = 0x6f)
+mh = Raspi_MotorHAT(addr=0x6f)
 myMotor = mh.getMotor(2)
 pwm = PWM(0x6F)
 pwm.setPWMFreq(60)
 
 sense = SenseHat()
 timer2 = None
+lock = Lock()
 
 signal.signal(signal.SIGINT, closeDB)
 polling()
